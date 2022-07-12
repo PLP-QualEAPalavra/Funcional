1c9ea64a9e36f1ce31a5b39ecdde2396afe86150
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-12 00:43:14 -0300

    feat: add word length verification

--- app/Sistemas individuais/termoo.hs
+++ app/Sistemas individuais/termoo.hs
@@ -4,9 +4,13 @@ termoo life word = do
   print("Tentativas restantes: " ++ show life )
   print(addSpace (hiddenWord word))
   tentativa <- getLine
+
+  if length tentativa > length word then putStrLn "Insira palavra do tamanho correto." 
+  else if life > 1 then termoo (life-1) word 
+  else putStrLn "Game Over" 
   -- verificaword tentativa word
-  if life > 1 then termoo (life-1) word
-  else putStrLn "Game Over"
+  -- if life > 1 then termoo (life-1) word
+  -- else putStrLn "Game Over"
 
 addSpace:: [Char] -> [Char]
 addSpace xs = if length xs == 1
