07bb76da9db46d174ac210c27d2a2200e71b68de
huandrey <huandrey.pontes@ccc.ufcg.edu.br>
2022-07-12 00:36:50 -0300

    feat: add addSpace and hiddenWord function to convert word in underlined

--- app/Sistemas individuais/termoo.hs
+++ app/Sistemas individuais/termoo.hs
@@ -1,17 +1,20 @@
 termoo :: Int -> String -> IO()
-termoo vidas palavra = do
+termoo life word = do
   print("--------------------------------------")
-  print("Tentativas restantes: " ++ show vidas )
-  print(escondePalavra palavra)
+  print("Tentativas restantes: " ++ show life )
+  print(addSpace (hiddenWord word))
   tentativa <- getLine
-  verificaPalavra tentativa palavra
-  if vidas > 1 then termoo (vidas-1) palavra
+  -- verificaword tentativa word
+  if life > 1 then termoo (life-1) word
   else putStrLn "Game Over"
 
-escondePalavra :: String -> String
-escondePalavra (h:t) = 
-  if t /= "" then ['_'] ++ [' '] ++ escondePalavra t 
-  else ['_'] ++ [' ']
+addSpace:: [Char] -> [Char]
+addSpace xs = if length xs == 1
+              then xs
+              else take (length xs - (length xs - 1)) xs ++ " " ++ addSpace (drop 1 xs)
+
+hiddenWord :: [Char] -> [Char]
+hiddenWord xs = ['_' | length xs /= 0, x <- xs];
 
 
 main :: IO()
