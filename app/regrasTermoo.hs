e27cce5f41f062d3ee862235eb1a1da58f184294
Maria Costa <maria.costa@youxgroup.com.br>
2022-07-14 22:32:58 -0300

    Dica das cores

--- app/regrasTermoo.hs
+++ app/regrasTermoo.hs
@@ -21,4 +21,9 @@ putStrLn " ou seja, se você acertar a palavra de primeira, pontua 150 pontos,"
 putStrLn " se acertar na segunda pontua 125 pontos e assim respectivamente.  "
 putStrLn " Ao final de cada palavra acertada, a pontuação é somada ao total de pontos. "
 putStrLn " Caso suas vidas acabem, o jogo será encerrado. "
+putStrLn " _______________________________________________ "
+putStrLn " Se a letra aparecer verde: ela está na palavra e na posição correta. "
+putStrLn " Se a letra aparecer amarela: ela está na palavra, mas a posição está incorreta. "
+putStrLn " Se a letra aparecer vermelha: ela não está na palavra. "
+
 
