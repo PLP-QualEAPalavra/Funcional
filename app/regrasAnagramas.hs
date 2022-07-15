d877ded3cf68a7fdcbdf4ea5de35c6b1a1774be6
Maysa Freire de Araújo <48970482+maysaaraujo@users.noreply.github.com>
2022-07-14 22:46:45 -0300

    Regras do modo anagrama

---
+++ app/regrasAnagramas.hs
@@ -0,0 +1,31 @@
+module Letreiros where
+import System.Process
+
+startanagrama :: IO()
+startanagrama = do
+  system "cls"
+  putStrLn " +______________________________________________________+ "
+  putStrLn " |                                                       |" 
+  putStrLn " |      █▀▀█ █▀█ █ █▀▀█ █▀▀▀ █▀▀█ █▀▀█ █▀▄ ▄▀█ █▀▀█      |"
+  putStrLn " |      █▄▄█ █ █ █ █▄▄█ █ ▄▄ █▀▀▄ █▄▄█ █  █  █ █▄▄█      |"
+  putStrLn " |      █  █ █ █▄█ █  █ █▄▄█ █  █ █  █ █  █  █ █  █      |"
+  putStrLn " |_______________________________________________________|"
+
+regrasAnagrama :: IO()
+regrasTermoo = do
+system "cls"
+putStrLn " +___________________________________________+ "
+putStrLn " O jogador pode tentar infinitas vezes contanto que "
+putStrLn " não não ultrapasse o tempo de 10 minutos. "
+putStrLn " Nesse modo existe um conjunto de palavras com caracteres "
+putStrLn " em comum e o jogador deve encontrar uma combinação que "
+putStrLn " que forme uma palavra. "
+putStrLn " A cada palavra acertada, você ganha 10 pontos, sendo "
+putStrLn " somado no seu total de pontos. "
+putStrLn " Quando acerta uma palavra em sequência, aumentará "
+putStrLn " um multiplicador, por exemplo com o acerto de uma palavra  "
+putStrLn " é alcançado 10 pontos, com o acerto de outra em seguida se "
+putStrLn " consegue 20 pontos. "
+putStrLn " A cada tentativa errada se perde 1 ponto.  "
+putStrLn " A partida acaba quando é alcançado o limite do tempo ou se "
+putStrLn " acertando todas as palavras do anagrama. "
