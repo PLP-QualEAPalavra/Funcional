384f50c4247c3d13e87f8f66e9fb15acd33f785e
Fabio Almendro <fabio.almendro@virtus.ufcg.edu.br>
2022-07-16 15:36:27 -0300

    ajustes no proejto e removendo oq não usamos

---
+++ Letreiros.hs
@@ -0,0 +1,85 @@
+module Letreiros where
+import System.Process
+
+gameOver :: IO()
+gameOver = do
+    system "cls"
+    putStrLn " +__________________________________________+ "
+    putStrLn " |                                          |" 
+    putStrLn " |         █▀█ █▀▀ █▀█ █▀▄ █▀▀ █ █          |" 
+    putStrLn " |         █▀▀ █▀  █▀▄ █ █ █▀  █ █          |"
+    putStrLn " |         ▀   ▀▀▀ ▀ ▀ ▀▀  ▀▀▀  ▀▀          |"
+    putStrLn " |__________________________________________|"
+
+
+timeIsOver :: IO()
+timeIsOver = do
+    system "cls"
+    putStrLn " +____________________________________________________________+ "
+    putStrLn " |                                                            |" 
+    putStrLn " |       ▀▀█▀▀ █ █▀▄ ▄▀█ █▀▀  █ █▀▀  █▀▀█ █   █ █▀▀ █▀█       |" 
+    putStrLn " |         █   █ █  █  █ █▀▀  █ ▀▀█  █  █  █▄█  █▀▀ █▀▄       |"
+    putStrLn " |         ▀   ▀ ▀  ▀  ▀ ▀▀▀  ▀ ▀▀▀  ▀▀▀▀   ▀   ▀▀▀ ▀ ▀       |"
+    putStrLn " |____________________________________________________________|"
+
+
+acerto :: IO()
+acerto = do
+  system "cls"
+  putStrLn " +__________________________________________+ "
+  putStrLn " |                                          |" 
+  putStrLn " |       █▀▀█ █▀▀▀ █▀▀ █▀█ ▀▀█▀▀ █▀▀█       |"
+  putStrLn " |       █▄▄█ █    █▀  █▀▄   █   █  █       |"
+  putStrLn " |       █  █ █▄▄▄ █▄▄ █ █   █   █▄▄█       |"
+  putStrLn " |__________________________________________|"
+
+startTermoo :: IO()
+startTermoo = do
+  system "cls"
+  putStrLn " +___________________________________________+ "
+  putStrLn " |                                           |" 
+  putStrLn " |      ▀▀█▀▀ █▀▀ █▀█ █▀▄ ▄▀█ █▀▀█ █▀▀█      |"
+  putStrLn " |        █   █▀  █▀▄ █  █  █ █  █ █  █      |"
+  putStrLn " |        ▀   ▀▀▀ ▀ ▀ ▀  ▀  ▀ ▀▀▀▀ ▀▀▀▀      |"
+  putStrLn " |___________________________________________|"
+
+startAnagrama :: IO()
+startAnagrama = do
+  system "cls"
+  putStrLn " +_______________________________________________________+ "
+  putStrLn " |                                                       |" 
+  putStrLn " |      █▀▀█ █▀█ █ █▀▀█ █▀▀▀ █▀▀█ █▀▀█ █▀▄ ▄▀█ █▀▀█      |"
+  putStrLn " |      █▄▄█ █ █ █ █▄▄█ █ ▄▄ █▀▀▄ █▄▄█ █  █  █ █▄▄█      |"
+  putStrLn " |      █  █ █ █▄█ █  █ █▄▄█ █  █ █  █ █  █  █ █  █      |"
+  putStrLn " |_______________________________________________________|"
+
+menuLetreiro :: IO()
+menuLetreiro = do
+  system "cls"
+  putStrLn " +___________________________________________+ "
+  putStrLn " |                                           |" 
+  putStrLn " |          █▀▄ ▄▀█ █▀▀ █▀▄  █ █ █           |"
+  putStrLn " |          █  █  █ █▀  █  ▀▄█ █ █           |"
+  putStrLn " |          ▀  ▀  ▀ ▀▀▀ ▀    ▀  ▀▀           |"
+  putStrLn " |___________________________________________|"
+
+
+recordLetreiro :: IO()
+recordLetreiro = do
+  system "cls"
+  putStrLn " +___________________________________________+ "
+  putStrLn " |                                           |" 
+  putStrLn " |      █▀█ █▀▀ █▀▀▀ █▀▀█ █▀█ █▀▄ █▀▀        |"
+  putStrLn " |      █▀▄ █▀  █    █  █ █▀▄ █ █ █▀         |"
+  putStrLn " |      ▀ ▀ ▀▀▀ ▀▀▀▀ ▀▀▀▀ ▀ ▀ ▀▀  ▀▀▀        |"
+  putStrLn " |___________________________________________|"
+
+creditosLetreiro :: IO()
+creditosLetreiro = do 
+  system "cls"
+  putStrLn " +___________________________________________+ "
+  putStrLn " |                                           |" 
+  putStrLn " |     █▀▀▀ █▀█ █▀▀ █▀▄ █ ▀▀█▀▀ █▀▀█ █▀▀▀    |"
+  putStrLn " |     █    █▀▄ █▀  █ █ █   █   █  █ ▀▀▀█    |" 
+  putStrLn " |     ▀▀▀▀ ▀ ▀ ▀▀▀ ▀▀  ▀   ▀   ▀▀▀▀ ▀▀▀▀    |"
+  putStrLn " |___________________________________________|"
